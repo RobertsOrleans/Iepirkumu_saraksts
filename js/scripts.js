e23f3ff6b9e374948b935e6061f6ae7681ca5e3c
RobertsOrleans <robertsorleans@gmail.com>
2021-04-20 16:54:45 +0300

    Var pievienot pirkumus

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,36 @@
+const ADD = document.getElementById('add');
 let pirkumi = [];
 
 window.addEventListener('load', () => {
-    console.log("Viss strādā!")
+    pirkumi = JSON.parse(localStorage.getItem("pirkumi") || "[]");
+    console.log(pirkumi)
+    render();
 });
 
-document.getElementById('add-button').addEventListener('click', () => {
+document.getElementById('addButton').addEventListener('click', () => {
     let pirkums = {produkts: produkts.value, daudzums: daudzums.value};
 
     produkts.value = "";
     daudzums.value = "";
 
-    gramatas.push(gramata);
+    pirkumi.push(pirkums);
 
     render();
 })
+
+function render() {
+    let saraksts = document.getElementById('saraksts');
+    saraksts.innerHTML = "";
+
+    for(let i = 0; i < pirkumi.length; i++) {
+        let pirkums = `
+        <div class="pirkums">
+            <h3 id="pirkums-text">${pirkumi[i].daudzums}</h3>
+            <h4>${pirkumi[i].produkts}</h4>
+        </div>`;
+
+        saraksts.innerHTML += pirkums;
+    }
+    
+    localStorage.setItem("pirkumi", JSON.stringify(pirkumi))
+}
