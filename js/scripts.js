ea0baa8226e9f1709aa17c65682844b69974b06f
RobertsOrleans <robertsorleans@gmail.com>
2021-04-27 13:30:52 +0300

    Centos tikt gala ar nonemsanu

--- js/scripts.js
+++ js/scripts.js
@@ -18,19 +18,37 @@ document.getElementById('addButton').addEventListener('click', () => {
     render();
 })
 
+function pirkumsRemove() {
+    let elements = document.getElementsByClassName('pirkums')
+
+    for (let index = 0; index < elements.length; index++) {
+        document.getElementsByClassName(index)[0].onclick = () => {
+            let saraksts = document.getElementById('saraksts');
+    
+            pirkumi.slice(elements[index],1);
+
+            render();
+
+            console.log(index);
+            console.log(pirkumi[index]);
+        }
+    }
+}
+
 function render() {
     let saraksts = document.getElementById('saraksts');
     saraksts.innerHTML = "";
 
     for(let i = 0; i < pirkumi.length; i++) {
         let pirkums = `
-        <div class="pirkums">
+        <div class="pirkums ${i}">
             <h3 id="pirkums-text">${pirkumi[i].daudzums}</h3>
             <h4>${pirkumi[i].produkts}</h4>
         </div>`;
 
         saraksts.innerHTML += pirkums;
+
+        pirkumsRemove();
     }
-    
     localStorage.setItem("pirkumi", JSON.stringify(pirkumi))
 }
